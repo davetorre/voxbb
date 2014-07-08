0bc4646646b19bac8a60a08026cf38e39e948199
Davis Hunt <davis@baydin.com>
2014-07-08 11:19:33 -0700

    Threaded sound and added slider to change temp.
    - Need to investigate the best way to start and manage threads (want to
    ensure that we do as little processing in the thread as possible).
    - Need to investigate the ‘lightest’ weight way to instantiate the
    individual sounds.
    - Let’s chat!

--- VoxBB/VoxBBViewController.h
+++ VoxBB/VoxBBViewController.h
@@ -12,12 +12,16 @@
 
 @interface VoxBBViewController : UIViewController
 
+@property (strong, nonatomic) IBOutlet UISlider *slider;
+@property (strong, nonatomic) IBOutlet UIButton *starter;
+
 @property (strong, nonatomic) AVAudioRecorder *audioRecorder;
 @property (strong, nonatomic) AVAudioPlayer *audioPlayer;
 
 - (IBAction)audioRecord:(UIButton *)sender;
 - (IBAction)audioStop:(UIButton *)sender;
 - (IBAction)audioPlay:(UIButton *)sender;
+- (IBAction)sliderValueChanged:(id)sender;
 
 @property (strong, nonatomic) NSMutableArray* audioTracks;
 @property (strong, nonatomic) NSMutableArray* audioTrackFiles;
--- VoxBB/VoxBBViewController.m
+++ VoxBB/VoxBBViewController.m
@@ -12,9 +12,16 @@
 
 @interface VoxBBViewController ()
 
+
 @end
 
+bool playMetronome = false;
+NSThread* metronome;
+uint64_t interval = (1000 * 1000 * 1000) / 2; //120 bpm
+uint64_t intervalMax = (1000 * 1000 * 1000);
+
 @implementation VoxBBViewController
+
 
 - (void)addTrack:(NSString*)filename
 {
@@ -28,33 +35,51 @@
 	
 	track.numberOfLoops = 0;
     
-	[self.audioTrackFiles addObject:filename];
-	[self.audioTracks addObject:track];
+	[_audioTrackFiles addObject:filename];
+	[_audioTracks addObject:track];
 }
 
 - (IBAction)startMachine:(UIButton *)sender
 {
-    self.audioTracks = [NSMutableArray arrayWithCapacity:10];
-    self.audioTrackFiles = [NSMutableArray arrayWithCapacity:10];
+    _audioTracks = [NSMutableArray arrayWithCapacity:10];
+    _audioTrackFiles = [NSMutableArray arrayWithCapacity:10];
     
     // You could do all mp3 or any other format supported by iOS software decoding.
     // Any format requiring the hardware will only work on the first track that starts playing.
+    
+    if (!playMetronome)
+    {
+        [sender setTitle:@"Stop Machine" forState:UIControlStateNormal];
+        playMetronome = true;
+        metronome = [[NSThread alloc] initWithTarget:self
+                                            selector:@selector(runMetronome)
+                                              object:nil];
+        [metronome start];
+    }
+    else
+    {
+        [sender setTitle:@"Start Machine" forState:UIControlStateNormal];
+        playMetronome = false;
+        [metronome cancel];
+    }
+    
+}
+
+- (void) runMetronome {
     [self addTrack:@"Kick707_2.mp3"];
     
     mach_timebase_info_data_t info;
     mach_timebase_info(&info);
     
+    
+    OALAudioTrack* track = [self.audioTracks objectAtIndex:0];
     uint64_t currentTime = mach_absolute_time();
     
     currentTime *= info.numer;
     currentTime /= info.denom;
     
-    uint64_t interval = (1000 * 1000 * 1000) / 2; //120 bpm
     uint64_t nextTime = currentTime + interval;
-    
-    OALAudioTrack* track = [self.audioTracks objectAtIndex:0];
-    
-    while (TRUE) {
+    while (playMetronome) {
         if (currentTime >= nextTime) {
             // Do some work, play the sound files or whatever you like
             NSLog(@"Current time: %lld", currentTime);
@@ -67,6 +92,12 @@
         currentTime = mach_absolute_time();
         currentTime *= info.numer;
         currentTime /= info.denom;
+    }
+}
+
+- (IBAction)sliderValueChanged:(id)sender {
+    if (sender == _slider) {
+        interval = intervalMax - _slider.value;
     }
 }
 
@@ -88,6 +119,10 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    _slider.maximumValue = interval*1.80;
+    _slider.value = interval;
+    _slider.minimumValue = interval/2;
 }
 
 - (void)didReceiveMemoryWarning
@@ -95,5 +130,4 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 @end
